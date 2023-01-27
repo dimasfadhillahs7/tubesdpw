45f2f8d5deba19b4faaff0ccd0bad77b1e926a1a
dimasfadhillahs7 <dimas.fadhillahs7@gmail.com>
2023-01-27 21:54:30 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,25 +1,21 @@
 
-function getJson() {
-  fetch('heroes.json')
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      let output = "";
-      data.forEach(function (hero) {
-        output += `<tr>
-                <td>${hero.name}</td>
-                <td>${hero.birth_year}</td>
-                <td>${hero.death_year}</td>
-                <td>${hero.description}</td>
-                <td>${hero.ascension_year}</td>
-                
-            </tr>`;
-      });
-      document.getElementById('tablebody').innerHTML = output;
+document.addEventListener('DOMContentLoaded', getJson());
+
+async function getJson() {
+  await fetch('https://indonesia-public-static-api.vercel.app/api/heroes')
+    .then(heroes => heroes.json())
+    .then(hero => {
+      new DataTable('#table_heroes', {
+        data: hero,
+        columns: [
+          { data: 'name', title: 'Nama' },
+          { data: 'birth_year', title: 'Tahun Lahir' },
+          { data: 'death_year', title: 'Tahun Wafat' },
+          { data: 'description', title: 'Deskripsi' },
+          { data: 'ascension_year', title: 'Tahun Pengangkatan' }
+          ],
+      })
     })
     .catch(err => console.log(err));
 }
-$(document).ready(function() {
-  getJson()
-});
-getJson()
+
